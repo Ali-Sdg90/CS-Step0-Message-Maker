4aa825f972b9a66d08f99ef0b083af747d4a1311
Ali Sadeghi <ali909392@gmail.com>
2024-07-07 18:40:23 +0330

    Add CustomScaleCalc.jsx / Improve main section styles / Before adding toastify

--- step0-message-maker/src/App.jsx
+++ step0-message-maker/src/App.jsx
@@ -6,64 +6,12 @@ import RemoveFromList from "./components/RemoveFromList/RemoveFromList";
 import Footer from "./components/Footer/Footer";
 import ShowList from "./components/ShowList/ShowList";
 import LoginSection from "./components/Login/LoginSection";
+import CustomScaleCalc from "./helpers/CustomScaleCalc";
 
 const App = () => {
-    const [pageWidth, setPageWidth] = useState(window.innerWidth);
-    const [pageHeight, setPageHeight] = useState(window.innerHeight);
-    const [scaleValue, setScaleValue] = useState(1);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setPageWidth(window.innerWidth);
-            setPageHeight(window.innerHeight);
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        handleResize();
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (pageHeight > pageWidth - 300) {
-            if (pageHeight / pageWidth > 1130 / 780) {
-                setScaleValue((1 * pageWidth) / 695);
-            } else if (
-                pageHeight / pageWidth < 915 / 616 &&
-                pageWidth > pageHeight
-            ) {
-                setScaleValue((0.5 * pageWidth) / 695);
-            } else {
-                setScaleValue((0.7 * pageWidth) / 695);
-            }
-        } else {
-            setScaleValue((1 * pageHeight) / 695);
-        }
-    }, [pageWidth, pageHeight]);
-
     return (
         <div className={Style.container}>
-            <div
-                className={`${
-                     pageHeight > pageWidth - 300
-                        ? Style.CustomScaleCalc
-                        : ""
-                }  ${Style.DefaultProperties}`}
-                style={
-                    pageHeight > pageWidth - 300
-                        ? {
-                              transform: `scale(${scaleValue}) rotate(90deg) translate(50%, -50%)`,
-                              top: `${(pageHeight - 1000 * scaleValue) / 2}px`,
-                          }
-                        : {
-                              transform: `scale(${scaleValue})`,
-                              width: `${pageWidth / scaleValue}px`,
-                          }
-                }
-            >
+            <CustomScaleCalc>
                 <Header />
                 <div className={Style.mainContents}>
                     <LoginSection />
@@ -74,7 +22,7 @@ const App = () => {
                     <ShowList />
                 </div>
                 <Footer />
-            </div>
+            </CustomScaleCalc>
         </div>
     );
 };
--- step0-message-maker/src/helpers/customScaleCalc.jsx
+++ step0-message-maker/src/helpers/customScaleCalc.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./CustomScaleCalc.scss";
 
-const customScaleCalc = ({children}) => {
+const CustomScaleCalc = ({ children }) => {
+    const [pageWidth, setPageWidth] = useState(window.innerWidth);
+    const [pageHeight, setPageHeight] = useState(window.innerHeight);
+    const [scaleValue, setScaleValue] = useState(1);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setPageWidth(window.innerWidth);
+            setPageHeight(window.innerHeight);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (pageHeight > pageWidth - 300) {
+            const aspectRatio = pageHeight / pageWidth;
+
+            if (pageHeight > pageWidth - 300) {
+                if (pageHeight / pageWidth > 1177 / 689) {
+                    setScaleValue((1 * pageWidth) / 695);
+                } else if (
+                    pageHeight / pageWidth < 915 / 616 &&
+                    pageWidth > pageHeight
+                ) {
+                    setScaleValue((0.5 * pageWidth) / 695);
+                } else {
+                    setScaleValue((0.65 * pageWidth) / 695);
+                }
+            } else {
+                setScaleValue((1 * pageHeight) / 695);
+            }
+        } else {
+            setScaleValue((1 * pageHeight) / 695);
+        }
+    }, [pageWidth, pageHeight]);
+
+    const containerClassName = () => {
+        return `${
+            pageHeight > pageWidth - 300 ? "CustomScaleCalc" : ""
+        } ${"DefaultProperties"}`;
+    };
+
     return (
-        <div>
-            
+        <div className={"container"}>
+            <div
+                className={containerClassName()}
+                style={
+                    pageHeight > pageWidth - 300
+                        ? {
+                              transform: `scale(${scaleValue}) rotate(90deg) translate(50%, -50%)`,
+                              top: `${(pageHeight - 1150 * scaleValue) / 2}px`,
+                          }
+                        : {
+                              transform: `scale(${scaleValue})`,
+                              width: `${pageWidth / scaleValue}px`,
+                          }
+                }
+            >
+                {children}
+            </div>
         </div>
     );
 };
 
-export default customScaleCalc;
+export default CustomScaleCalc;
